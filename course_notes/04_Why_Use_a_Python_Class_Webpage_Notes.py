3ee2ffc6436b676b0927e10eea13c62aa2875cd9
franpanteli <francescapanteli@gmail.com>
2025-11-05 23:10:15 +0000

    Automated commit at 2025-11-05 23:10:15

--- course_notes/04_Why_Use_a_Python_Class_Webpage_Notes.py
+++ course_notes/04_Why_Use_a_Python_Class_Webpage_Notes.py
@@ -1,3 +1,43 @@
 """ Why Use a Python Class Webpage Notes 
+    -> outline
+        -> introduction
+        -> cooking with Python
+            -> without a class
+        -> summary: why use a Python class
+    -> introduction
+        -> previous <- OOP high-level theoretical view
+        -> creating classes
+    -> cooking with Python
+        -> building an Ingredient class
+        -> building soup from this class
+        -> cooking ingredients
+        -> without a class
+            -> storing the ingredients in a list
+"""
 
+ingredients = ["carrot", "pea", "squash"]
+
+"""
+    -> this is for only storing names of the ingredients 
+        -> we need methods
+    -> we also need more information, like the amount of each ingredient 
+        -> we can use a dictionary to do this
+        -> mapping the name of each ingredient to a list / other dictionary that contains more information about the 
+            food item  
+"""
+
+ingredients = {
+                'carrots': {'name': 'carrot', 'amount': 3},
+                'peas': {'name': 'pea', 'amount': 12}
+              }
+
+"""
+    -> this dictionary contains the names and the amount of each ingredient type 
+    -> we can then use this information to write a recipie 
+"""
+
+print(f"Let's take {ingredients['carrots']['amount']} of {ingredients['carrots']['name']} and {ingredients['peas']['amount']} of {ingredients['peas']['name']}.")
+
+"""
+    -> 
 """
