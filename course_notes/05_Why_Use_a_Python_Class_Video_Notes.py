396a80519d4d05036efba57b5fa43cea9b564918
franpanteli <francescapanteli@gmail.com>
2025-11-05 23:19:08 +0000

    Automated commit on 2025-11-05 23:18:43

--- course_notes/05_Why_Use_a_Python_Class_Video_Notes.py
+++ course_notes/05_Why_Use_a_Python_Class_Video_Notes.py
@@ -1,5 +1,6 @@
 """ Why Use a Python Class Video Notes 
     -> outline
         -> benefits of Python classes
-        -> creating our own Python classes 
+        -> creating our own Python classes
+    -> 
 """
