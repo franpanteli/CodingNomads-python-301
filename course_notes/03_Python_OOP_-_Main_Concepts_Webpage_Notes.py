4b70823cbd2bf3410c9f2eeccabdb3c80b3ab74b
franpanteli <francescapanteli@gmail.com>
2025-11-05 14:52:18 +0000

    Automated commit at 2025-11-05 14:52:18

--- course_notes/03_Python_OOP_-_Main_Concepts_Webpage_Notes.py
+++ course_notes/03_Python_OOP_-_Main_Concepts_Webpage_Notes.py
@@ -1,3 +1,13 @@
 """ Python OOP - Main Concepts Webpage Notes 
-
+    -> outline 
+        -> introduction 
+        -> four traits of OOP 
+            -> 1. abstraction 
+            -> 2. encapsulation 
+            -> 3. inheritance 
+            -> 4. polymorphism 
+            -> 5. composition 
+            -> additional resources 
+        -> summary: Python OOP main concepts
+            -> OOP paradigm 
 """
