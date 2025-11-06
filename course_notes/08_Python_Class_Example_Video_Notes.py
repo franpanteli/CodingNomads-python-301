ad01c5e85fd50cce671559c0ac04af0c3ef92ecb
franpanteli <francescapanteli@gmail.com>
2025-11-06 18:44:43 +0000

    Automated commit at 2025-11-06 18:44:43

--- course_notes/08_Python_Class_Example_Video_Notes.py
+++ course_notes/08_Python_Class_Example_Video_Notes.py
@@ -1,5 +1,12 @@
 """ Python Class Example Video Notes 
     -> outline 
         -> modelling ingredient objects
-    -> 
+    -> what a class is / how little you need to create  one
 """
+
+#defining the class 
+class Ingredient: #use the singular form
+    pass
+
+#making an instance of the class
+ingredient = Ingredient() #CLASSES ARE WRITTEN IN TITLE CASE
