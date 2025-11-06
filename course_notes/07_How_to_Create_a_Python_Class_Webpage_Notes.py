75a1fbaa088e1beda0c36e98d064056efb747402
franpanteli <francescapanteli@gmail.com>
2025-11-06 18:38:39 +0000

    Automated commit at 2025-11-06 18:38:39

--- course_notes/07_How_to_Create_a_Python_Class_Webpage_Notes.py
+++ course_notes/07_How_to_Create_a_Python_Class_Webpage_Notes.py
@@ -1,3 +1,58 @@
 """ How to Create a Python Class Webpage Notes 
+    -> outline 
+        -> introduction
+        -> define a class
+            -> class name 
+            -> docstring
+            -> indentation 
+        -> instantiate a class
+            -> how to instantiate a class
+            -> what is instantiating a class
+            -> additional resources 
+        -> summary: how to create a class
+            -> clss definition
+            -> syntax
+    -> define a class 
+        -> to instantiate a class 
+"""
+
+class ClassName:
+    """Docstring"""
+    pass
 
 """
+    -> we define a class with the class keyword
+    -> this is in singular
+    -> CLASSES ARE NAMED WITH THIS CONVENTION: ClassName
+        -> THIS HELPS US RECOGNISE THEM AS CLASSES
+        -> you can also add a docstring (three line comment) when writing the class
+        -> this is the documentation for the class
+    -> attributes and methods are defined inside the class definition 
+    -> creating instances of the class
+        -> instantiating the class 
+"""
+
+i = Ingredient()
+
+d = dict()
+
+"""
+    -> the most common types of object names are lowercase
+    -> next <- looking at what we can do without defining methods 
+    -> summary 
+        -> a constructor is used to instantiate an object from a class
+        -> the name of a class is ƒollowed by parenthesis is the constructor 
+        -> the output of a constructor can be set to a variable 
+    -> class definition 
+        -> the class keyword
+        -> THE NAME OF THE CLASS IN CapCase AND SINGULAR
+        -> the colon ;
+        -> docstring of the class, indented by four spaces
+        -> the body of the class definition, containing all the code for the class 
+            -> THE CODE FOR THE BODY OF THE CLASS NEEDS TO BE INDENTED BY FOUR SPACES
+        -> syntax 
+"""
+
+class ClassName:
+    """Docstring"""
+    pass
